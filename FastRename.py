9cba68bf610c1021493fb1e0578ff6669a8fa28e
Lenjis <rzxhandsome@sina.com>
2024-12-06 19:53:38 +0800

    modified:   FastRename.py 	modified:   __pycache__/FastRename.cpython-312.pyc 	deleted:    "\345\274\271\345\212\233.pdf"

--- FastRename.py
+++ FastRename.py
@@ -10,7 +10,7 @@ def Merge(path1: str, path2: str):
     Parameters:
     path (str, optional): The directory path to be processed.
     """
-    print("Merging files...")
+
     try:
         for filename in os.listdir(os.path.join(path1, "input")):
             shutil.move(
@@ -20,7 +20,6 @@ def Merge(path1: str, path2: str):
             shutil.move(
                 os.path.join(path1, "output", filename), os.path.join(path2, "output")
             )
-        print("Files merged.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -30,15 +29,12 @@ def RemoveIndex(path="./"):
     Parameters:
     path (str, optional): The directory path to be processed. Defaults to the current directory.
     """
-    print("Removing index...")
     try:
         for filename in os.listdir(path):
             match = re.search(pattern, filename)
             if match:
                 newname = filename.replace(match.group(), "", 1)
-                print(match.group())
                 os.rename(os.path.join(path, filename), os.path.join(path, newname))
-        print("All indexes removed.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -50,7 +46,6 @@ def AddIndex(path="./", start_idx=1, end_idx=None):
     end_idx (int, optional): The ending index. If not provided, indexes will be added to all files.
     """
     idx = start_idx
-    print("Adding index...")
     try:
         if end_idx == None:
             for filename in os.listdir(path):
@@ -69,12 +64,11 @@ def AddIndex(path="./", start_idx=1, end_idx=None):
                             os.path.join(path, filename), os.path.join(path, newname)
                         )
                         idx += 1
-        print("Index added.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
-def Rename(path1: str, path2: str, start_idx=1, end_idx=None):
+def MergeAndRename(path1: str, path2: str, start_idx=1, end_idx=None):
     """
     Parameters:
     path1 (str): The directory path to be processed.
@@ -82,10 +76,12 @@ def Rename(path1: str, path2: str, start_idx=1, end_idx=None):
     start_idx (int, optional): The starting index. Defaults to 1.
     end_idx (int, optional): The ending index. If not provided, indexes will be added to all files.
     """
-
+    print("Merging files...")
     Merge(path1, path2)
+    print("Removing the index...")
     RemoveIndex(os.path.join(path2, "input"))
     RemoveIndex(os.path.join(path2, "output"))
+    print("Re-adding the index...")
     AddIndex(os.path.join(path2, "input"), start_idx, end_idx)
     AddIndex(os.path.join(path2, "output"), start_idx, end_idx)
 
@@ -93,4 +89,11 @@ def Rename(path1: str, path2: str, start_idx=1, end_idx=None):
     last_name1 = names1[-1]
     names2 = path2.split("_")
     last_name2 = names2[-1]
-    os.rename(path2, path2.replace(last_name2, str(last_name1 + last_name2)))
+    ans = int(last_name1) + int(last_name2)
+    print(ans)
+    if last_name2.isdigit():
+        os.rename(
+            path2, path2.replace(
+                last_name2, str(int(last_name1) + int(last_name2))
+            )
+    )
